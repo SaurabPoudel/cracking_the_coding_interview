074993c7634a03067ac1e905bdb54fe8094328cf
SaurabPoudel <poudelsaurab20@gmail.com>
2024-04-21 17:06:20 +0545

    optimized code for one edit away

--- ArrayAndStrings/OneEditAway.cpp
+++ ArrayAndStrings/OneEditAway.cpp
@@ -60,9 +60,45 @@ bool oneEditAway(string first, string second)
     return false;
 }
 
+// since oneEditReplace and oneEditInsert are very similar , we can merge them into one method
+
+bool oneEditAway2(string first, string second)
+{
+    if ((abs(static_cast<long>(first.length())) - abs(static_cast<long>(second.length()))) > 1)
+    {
+        return false;
+    }
+    string s1 = first.length() < second.length() ? first : second;
+    string s2 = first.length() < second.length() ? second : first;
+
+    bool foundDifference = false;
+    int index1 = 0, index2 = 0;
+
+    while (index2 < s2.length() && index1 < s1.length())
+    {
+        if (s1[index1] != s2[index2])
+        {
+            // first difference found
+            if (foundDifference)
+                return false;
+            foundDifference = true;
+            if (s1.length() == s2.length())
+            {
+                index1++;
+            }
+        }
+        else
+        {
+            index1++;
+        }
+        index2++;
+    }
+    return true;
+}
+
 int main()
 {
     string first = "pale";
     string second = "padle";
-    cout << oneEditAway(first, second);
+    cout << oneEditAway2(first, second);
 }
